fb9a8dc4d06912fa8dc1fc1eb7cf4f1b033f8175
jiho <rlagh8515@gmail.com>
2022-11-16 13:54:22 +0900

    sowe

--- login/main.py
+++ login/main.py
@@ -1,24 +1,6 @@
-import sys
-import os
 from PyQt5 import QtWidgets, QtSql
 from login import *
-from soweMusic import *
-
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        QMainWindow.__init__(self)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        loadJsonStyle(self, self.ui)
-
-        self.show()
-        self.ui.settingsBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
-        self.ui.infoBtn.clicked.connect(lambda: self.ui.centerMenuContainer.expandMenu())
-        self.ui.closeCenterMenuBtn.clicked.connect(lambda: self.ui.centerMenuContainer.collapseMenu())
-
-        self.ui.moreMenuBtn.clicked.connect(lambda: self.ui.rightLoginContainer.expandMenu())
-        self.ui.loginBtn.clicked.connect(lambda: self.ui.rightLoginContainer.expandMenu())
-        self.ui.closeLoginBtn.clicked.connect(lambda: self.ui.rightLoginContainer.collapseMenu())
+import sys
 
 class loginWindow(QMainWindow):
     def __init__(self):
@@ -50,9 +32,7 @@ class loginWindow(QMainWindow):
         self.query.exec_("select * from userdata where username = '%s' and password = '%s';"%(username1,password1))
         self.query.first()
         if self.query.value("username") != None and self.query.value("password") != None:
-            Window = MainWindow()
-            self.close()
-            Window.show()
+            print("Login successful!")
         else:
             print("Login failed!")
     
@@ -70,10 +50,3 @@ if __name__ == "__main__":
 
     Window.show()
     sys.exit(app.exec_())
-
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     Window = MainWindow()
-
-#     Window.show()
-#     sys.exit(app.exec_())
